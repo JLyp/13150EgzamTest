4396bf716dff70ecf44efa610d5189e507550d42
JLyp <jakublypik@gmail.com>
2022-02-02 11:24:31 +0100

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,89 @@
+﻿using EgzamTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgzamTest.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static IList<TaskModel> tasks = new List<TaskModel>()
+        {
+          new TaskModel(){ TaskId = 1, Weigh = 55, Heigh = 54, Price = 15, Number = 45, Done = false},
+          new TaskModel(){ TaskId = 1, Weigh = 55, Heigh = 54, Price = 15, Number = 45, Done = false}
+        };
+
+        // GET: TaskController
+        public ActionResult Index()
+        {
+            return View(tasks);
+        }
+
+        // GET: TaskController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TaskController/Create
+        public ActionResult Create()
+        {
+            return View(new TaskModel());
+        }
+
+        // POST: TaskController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskModel taskModel)
+        {
+            taskModel.TaskId = tasks.Count + 1;
+            tasks.Add(taskModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TaskController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgzamTest.Models
+{
+    public class TaskModel
+    {
+        public  int TaskId { get; set; }
+        [DisplayName("Szerokość")]
+        [Required(ErrorMessage = "Pole Nazwa jest wymagane.")]
+        [MaxLength(50)]
+        public int Weigh { get; set; }
+        [DisplayName("Wysokość")]
+        [MaxLength(50)]
+        public int Heigh { get; set; }
+        
+        [DisplayName("Cena")]
+        [MaxLength(50)]
+        public decimal Price { get; set; }
+
+        [DisplayName("Liczba")]
+        [MaxLength(50)]
+        public int Number { get; set; }
+        public bool Done { get; set; }
+    }
+}
